e07f411666c33e67b2e7dc03dfb6b02fb1c5a3c9
melon <momobp114@gmail.com>
2022-08-01 13:59:52 +0700

    Add JWT Authentication security

---
+++ src/main/java/th/ac/ku/menu/security/AudienceValidator.java
@@ -0,0 +1,26 @@
+package th.ac.ku.menu.security;
+
+import org.springframework.security.oauth2.core.*;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import java.util.List;
+
+public class AudienceValidator implements OAuth2TokenValidator<Jwt> {
+    private final String audience;
+
+    AudienceValidator(String audience) {
+        this.audience = audience;
+    }
+
+
+    @Override
+    public OAuth2TokenValidatorResult validate(Jwt jwt) {
+        List<String> audiences = jwt.getAudience();
+
+        if(audiences.contains(this.audience)) {
+            return OAuth2TokenValidatorResult.success();
+        }
+        OAuth2Error err = new OAuth2Error(OAuth2ErrorCodes.INVALID_TOKEN);
+        return OAuth2TokenValidatorResult.failure(err);
+    }
+}
